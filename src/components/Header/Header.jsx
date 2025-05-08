66ee6aed99c0189731099941d8a3bd15d94413bc
kekc <kekcsik009@gmail.com>
2025-05-08 21:25:46 +0700

    style header underline

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import * as S from './Header.styled.js'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
+    const [isUsed, setIsUsed] = useState('true') //активная страница
     return (
         <S.Header>
             <S.HeaderLogoImg />
             <S.MenuList>
                 <S.MenuItem>
                     <Link>
-                        <S.HeadExpenses to="/">Мои расходы</S.HeadExpenses>
+                        <S.HeadExpenses style={{ textDecoration: isUsed ? 'underline' : 'none' }} to="/">Мои расходы</S.HeadExpenses>
                     </Link>
                 </S.MenuItem>
                 <S.MenuItem>
